ed86c3d0a82ef6b8d7890566bf1d3758137407e9
sexyapy <82424011+sexyapy@users.noreply.github.com>
2021-04-13 09:13:26 +0000

    Add files via upload

---
+++ PancakeBank.sol
@@ -0,0 +1,219 @@
+// File: @openzeppelin\contracts\token\ERC20\IERC20.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+
+// File: contracts\libraries\SafeMath.sol
+
+pragma solidity =0.6.12;
+
+// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+
+library SafeMathUniswap {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+}
+
+// File: contracts\IBankDistributor.sol
+
+pragma solidity 0.6.12;
+
+
+interface IBankDistributor {
+    // Bank info
+    function rewardToken() external view returns (IERC20);
+    function poolCount() external view returns (uint256);
+    function sellableRewardAmount() external view returns (uint256);
+    
+    // Pool info
+    function lockableToken(uint256 poolId) external view returns (IERC20);
+    function lockedAmount(address user, uint256 poolId) external view returns (uint256);
+    
+    // Pool actions, requires impersonation via delegatecall
+    function deposit(address adapter, uint256 poolId, uint256 amount) external;
+    function withdraw(address adapter, uint256 poolId, uint256 amount) external;
+    function claimReward(address adapter, uint256 poolId) external;
+    
+    function emergencyWithdraw(address adapter, uint256 poolId) external;
+    
+    // Service methods
+    function poolAddress(uint256 poolId) external view returns (address);
+
+    // Governance info methods    
+    function lockedValue(address user, uint256 poolId) external view returns (uint256);
+    function totalLockedValue(uint256 poolId) external view returns (uint256);
+    function normalizedAPY(uint256 poolId) external view returns (uint256);
+}
+
+pragma solidity 0.6.12;
+
+
+interface IMasterChef{
+    function poolInfo(uint256) external view returns (IERC20,uint256,uint256,uint256);
+    function userInfo(uint256, address) external view returns (uint256,uint256);
+    function poolLength() external view returns (uint256);
+    function pendingSushi(uint256 _pid, address _user) external view returns (uint256);
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function withdraw(uint256 _pid, uint256 _amount) external;
+    function emergencyWithdraw(uint256 _pid) external;
+}
+
+
+pragma solidity 0.6.12;
+
+
+contract PancakeBank is IBankDistributor {
+    IMasterChef constant cakeMasterchef = IMasterChef(0x73feaa1eE314F8c655E354234017bE2193C9E24E);
+    IERC20 constant cake = IERC20(0x4c6d40a7a88beE8690949Dc7EE6Fd0cf22C29603);
+    address public devadr;
+
+    constructor() public {
+        devadr = msg.sender;
+    }
+
+    // Bank info
+    function rewardToken() external view override returns (IERC20) {
+        return cake;
+    }
+
+    function poolCount() external view override returns (uint256) {
+        return cakeMasterchef.poolLength();
+    }
+
+    function sellableRewardAmount() external view override returns (uint256) {
+        return uint256(-1);
+    }
+
+    
+    // Pool info
+    function lockableToken(uint256 poolId) external view override returns (IERC20) {
+        (IERC20 lpToken,,,) = cakeMasterchef.poolInfo(poolId);
+        return lpToken;
+    }
+
+    function lockedAmount(address user, uint256 poolId) external view override returns (uint256) {
+        (uint256 amount,) = cakeMasterchef.userInfo(poolId, user);
+        return amount;
+    }
+    
+    // Pool actions, requires impersonation via delegatecall
+    function deposit(address _adapter, uint256 poolId, uint256 amount) external override {
+        IBankDistributor adapter = IBankDistributor(_adapter);
+        adapter.lockableToken(poolId).approve(address(cakeMasterchef), uint256(-1));
+        cakeMasterchef.deposit( poolId, amount);
+    }
+
+    function withdraw(address, uint256 poolId, uint256 amount) external override {
+        cakeMasterchef.withdraw( poolId, amount);
+    }
+
+    function claimReward(address, uint256 poolId) external override {
+        cakeMasterchef.deposit( poolId, 0);
+    }
+    
+    function emergencyWithdraw(address, uint256 poolId) external override {
+        cakeMasterchef.emergencyWithdraw(poolId);
+    }   
+    
+    // Service methods
+    function poolAddress(uint256) external view override returns (address) {
+        return address(cakeMasterchef);
+    }
+    
+    function lockedValue(address, uint256) external override view returns (uint256) {
+        require(false, "not implemented");
+    }    
+
+    function totalLockedValue(uint256) external override view returns (uint256) {
+        require(false, "not implemented"); 
+    }
+
+    function normalizedAPY(uint256) external override view returns (uint256) {
+        require(false, "not implemented");
+    }
+}
